0e817162ca33af43f829e779954eba26332648f0
devops-school <44023974+devops-school@users.noreply.github.com>
2019-10-29 11:00:26 +0530

    dasd
    ada

---
+++ rajeshkumar/github_resources.tf
@@ -0,0 +1,13 @@
+resource "github_repository" "web" {
+  name        = "allianzday2"
+  description = "My awesome codebase"
+
+  private = false
+}
+
+resource "github_repository" "web1" {
+  name        = "allianzday2"
+  description = "My awesome codebase"
+
+  private = false
+}

f87205cdb6cdbb19ebd6f2fa9ee4ad3d1cfd2a01
devops-school <44023974+devops-school@users.noreply.github.com>
2019-10-30 12:51:18 +0530

    Add files via upload

---
+++ rajeshkumar/terraform-variable/terraform-variable-type-list-count-for.tf
@@ -0,0 +1,17 @@
+variable "names" {
+  description = "A list of names"
+  type        = list(string)
+  default     = ["rajesh", "kumar", "xyz"]
+}
+# Alternate of for each using count 
+resource "github_repository" "repox" {
+  count = "${var.numofusers}"
+  name  = "rajesh.${count.index}"
+  description = "My awesome codebase"
+  private = false
+}
+
+# using for-each
+output "upper_names" {
+  value = [for name in var.names : upper(name) if length(name) < 5]
+}

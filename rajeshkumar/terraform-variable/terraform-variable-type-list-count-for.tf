c2267a4d42034d06ff253c1f9aa54175bed2f72a
devops-school <44023974+devops-school@users.noreply.github.com>
2019-10-30 12:53:40 +0530

    dsad
    ad

--- rajeshkumar/terraform-variable/terraform-variable-type-list-count-for.tf
+++ rajeshkumar/terraform-variable/terraform-variable-type-list-count-for.tf
@@ -5,7 +5,7 @@ variable "names" {
 }
 # Alternate of for each using count 
 resource "github_repository" "repox" {
-  count = "${var.numofusers}"
+  count = "${var.names}"
   name  = "rajesh.${count.index}"
   description = "My awesome codebase"
   private = false
